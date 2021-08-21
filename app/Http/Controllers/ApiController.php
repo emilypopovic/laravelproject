1c57e1181f015db82c3e553e1517dab7b88afeff
Emily Popovic <epopovic@uwo.ca>
2021-08-21 12:54:54 -0400

    update controller

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class ApiController extends Controller
@@ -11,7 +12,14 @@ class ApiController extends Controller
     }
 
     public function createStudent(Request $request) {
-        // logic to create a student record goes here
+        $student = new Student();
+        $student->name = $request->name;
+        $student->course = $request->course;
+        $student->save();
+
+        return response()->json([
+            "message" => "student record created"
+        ], 201);
     }
 
     public function getStudent($id) {
